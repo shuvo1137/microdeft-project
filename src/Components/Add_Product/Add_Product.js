f61dafcb9188d14e8277eb725dca3f2e8f3bc0a6
shuvo1137 <shuvo2220@gmail.com>
2023-06-03 17:04:00 +0600

    delete function and others

--- src/Components/Add_Product/Add_Product.js
+++ src/Components/Add_Product/Add_Product.js
@@ -3,7 +3,14 @@ import React from "react";
 const Add_Product = () => {
   return (
     <div>
+      <h1>Adding Products</h1>
       <input type="text" placeholder="Product-Name" name="" id="" />
+      <input type="text" placeholder="Description" name="" id="" />
+      <input type="text" placeholder="Category" name="" id="" />
+      <input type="text" placeholder="imageURL" name="" id="" />
+      <input type="text" placeholder="Price" name="" id="" />
+
+      <button>Add Products</button>
     </div>
   );
 };
